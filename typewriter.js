7d5f49ac90f728c78e8004a3c2cdcb2bd44be79e
robotsch <christianhumble22@gmail.com>
2022-03-15 11:27:53 -0400

    remove extra newline, add comments

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,9 @@
 const sentence = "hello there from lighthouse labs";
 
+// Print after full delay of printing sentence has finished
 setTimeout(() => process.stdout.write("\n"), sentence.length * 50);
 for (let i = 0; i < sentence.length - 1; i++) {
+  // Print each character with a delay calculated by the loop
   setTimeout(() => process.stdout.write(sentence[i]), i * 50);
 }
-process.stdout.write("\n");
+
